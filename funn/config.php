bbd363bad8868c5b885117ccfbe87c66487585d0
lord.xeon@gmail.com <lord.xeon@gmail.com@d8b2c144-dc4b-914c-d878-6766efc34fb8>
2011-02-05 22:32:02 +0000

    year to date stuff

--- funn/config.php
+++ funn/config.php
@@ -6,32 +6,53 @@
 	3. Navigate to the directory on your domain adding stats-YOUR.DOMAIN.COM to the URL
 	4. Enjoy
 */
+
 $statsURL = $_GET['stats'];
 $dayR = $_GET['time'];
 $tday = date('mY');
+
 //if the time is not specified, then use the current month and year
 if($dayR==''):
 	$dayR = date('mY');
 endif;
 
 /*nordlich version number*/
-$nordlich  = '0.3.2';
+$nordlich  = '0.4';
 
 /*Abslolute server path to AWStats data files*/
 $stsP = '/var/lib/awstats/';
 
+//default AWStats file name
 $stsF = $stsP.'awstats'.$dayR.'.'.$statsURL.'.txt';
-//sample AW stats output file
-if(file_exists($stsF)):
-	$sts = file($stsF);
-	require_once('funn/funnns.php');
-	require_once('funn/metadata.php');
-	require_once('funn/data.php');
-	require_once('funn/chartmeta.php');
-	require_once('tableArgs.php');
 
-	$stsList = fileList($stsP);
-else:
-	$sts = false;
-endif;
+//domain
+$domain = 'deadlycomputer.com';
+
+//Year to date functionality, shows the monthly data from Jan. to Current month
+$year2Date = true;
+//year to date savefile directory by default it is in the funn/y2d/YEAR
+$y2dDir = '/home/alphavega/demo/ph/nordlicht/funn/y2d/'.date('Y');
+$y2d = $y2dDir .'/'.$statsURL.'.txt';
+
+require_once('funnns.php');
+//list of subdomains
+$stsList = fileList($stsP);
+
+//splash page check
+if($_SERVER['PHP_SELF'] != 'nordlicht/index.php'):
+	//sample AW stats output file
+	if(file_exists($stsF)):
+		$sts = file($stsF);
+		require_once('metadata.php');
+		require_once('data.php');
+		require_once('chartmeta.php');
+		require_once('tableArgs.php');
+	else:
+		$sts = false;
+	endif;	
+	//Year to date
+	if($year2Date == true):
+		require_once('funn/y2d/y2d-builder.php');
+	endif;
+endif;	//end splash page if
 ?>
