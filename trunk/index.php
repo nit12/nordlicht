769f6134d03a64d7e46a6394f58be1f0eab20c97
lord.xeon@gmail.com <lord.xeon@gmail.com@d8b2c144-dc4b-914c-d878-6766efc34fb8>
2011-02-06 22:23:57 +0000

    made splash page nicer

--- trunk/index.php
+++ trunk/index.php
@@ -9,20 +9,44 @@
 	<link type="text/css" rel="stylesheet" href="funn/css/ruthless.css"/>
 	<script src="funn/js/modernizr-1.6.min.js"></script>
 	<style>
-		body { background:#fff; }
-		header { }
+		body { width:80%; margin:0 auto; font-size:24px; font-family:"PT Sans", Verdana, Geneva, sans-serif; color:#fff; }
+		h1 {
+			text-align:center;
+			text-shadow:2px 2px 4px rgba(167,231,215,0.9),
+						4px 4px 8px rgba(167,231,215,0.5),
+						8px 8px 12px rgba(167,231,215,0.1);
+			color:rgba(167,213,215,0.8);
+		}
+		
+		section { 
+			margin:0 auto;
+			width:600px;
+		}
+		img { margin:0 auto; text-align:center; display:block; }
+		footer {
+			margin:50px auto;
+			background:rgba(236,166,192,0.9);
+			-webkit-box-shadow:2px 2px 4px rgba(236,166,192,0.7),
+							4px 4px 8px rgba(236,166,192,0.3),
+							inset 2px 2px 4px rgba(201,121,151,0.7),
+							inset 4px 4px 8px rgba(201,121,151,0.3);
+			text-align:center;
+			padding:5px;
+		}
 	</style>
 </head>
 <body>
 	<header>
 		<h1>AWStats using nordlicht for: <?php echo $domain;?></h1>
 	</header>
-	<section>
+	<section id="splashPage">
+		<img src="funn/img/others/aurora.jpg" alt="northern lights" />
 		<p>Welcome to the stats section of <?php echo $domain; ?>.  Please choose a subdomain, and date below.</p>
 		<div>
 			<?php fileDrop($stsList);?>
+			<br><br>
 			<input type="text" id="dayR" name="dayR" />
-			<a href="juno.php" title="Go to Stats" id="goToStats">Go!</a>
+			<a href="juno.php" title="Go to Stats" id="goToStats" class="goButton">Go!</a>
 		</div>
 	</section>
 	<footer>
@@ -30,7 +54,7 @@
     </footer>
 </div> <!--! end of #container -->
 <script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/1.4.4/jquery.min.js"></script>
-<script type="text/javascript" src="funn/js/ultra.min.js"></script>
+<script type="text/javascript" src="funn/js/ultra.js"></script>
 <script type="text/javascript">
 	$("#goToStats").click(function(e){
 		e.preventDefault();
