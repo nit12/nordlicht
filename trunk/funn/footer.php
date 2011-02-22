7cb8ef187b010e6327bd99ed35bac71724651fca
lord.xeon@gmail.com <lord.xeon@gmail.com@d8b2c144-dc4b-914c-d878-6766efc34fb8>
2011-02-22 02:27:23 +0000

    jquery 1.5

--- trunk/funn/footer.php
+++ trunk/funn/footer.php
@@ -1,8 +1,9 @@
 	<footer>
 		Made with <a href="http://awstats.org/" title="AWStats"><?php echo $sts[0];?></a> using <a href="http://www.alphavega.com/nordlicht/" title="nordlicht">nordlich version <?php echo $nordlich; ?></a>.
     </footer>
+	<div id="fullRange"></div>
 </div> <!--! end of #container -->
-<script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/1.4.4/jquery.min.js"></script>
+<script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/1.5/jquery.min.js"></script>
 <script type="text/javascript" src="funn/js/ultra.js"></script>
 </body>
 </html>
