d1b4e8bbbe40f5f50720a34f718228082b54dca5
lord.xeon@gmail.com <lord.xeon@gmail.com@d8b2c144-dc4b-914c-d878-6766efc34fb8>
2011-04-20 02:29:11 +0000

    moved flot options metadata to seperate file - ju87.js changed amo to superfortress.js

---
+++ js/ju87.js
@@ -0,0 +1,221 @@
+/*
+ * Junkers Ju 87
+ * Flot metadata for all charts of nordlicht Omaha build
+ *
+ *	Table of Contents:
+ *	@1. Browsers
+ *		@1.1 Minor Browsers
+ */
+var flotOps = {
+	/*@1 Browsers*/
+	browser: {
+		series: {
+			pie: {
+				show:true,	//we want to show a pie chart
+				radius:0.8,	//we want it to be smaller then the container, to get a better donut effect when you get the minor versions
+				stroke: {
+					color:'#fff',	//inner borders white
+					width:2
+				},
+				label: {
+					show:false,
+					radius:0.8,
+					background: {
+						opacity:0.2
+					},
+					threshold:0.05
+				},
+				combine: {
+					threshold:0.05,		//combine all slices that are under 5% of total
+					color:"#333333",	//make them color #333333
+					label:'All Others'
+				}
+			}
+		},
+		legend: {
+			show:true,		//show the legend
+			container: $("#browser figcaption"),	//put the legend in the figcaption under the browser section
+			noColumns: 1,
+			labelFormatter: function(label, series){
+				var ll = '',
+					lc = label.charAt(0).toUpperCase() + label.substr(1);
+				if(label == 'msie') {
+					lc = 'IE';
+				}
+				ll = '<span class="pie '+label+'"></span>'+lc + " - "+Math.round(series.percent)+"%";
+				return ll;
+			}
+		},
+		grid: {
+			hoverable:true,
+			clickable:true
+		}
+	},
+	//Browser minor metadata
+	browserMinor: {
+		series: {
+			pie: {
+				innerRadius:0.55,
+				radius:0.9,
+				show:true,
+				stroke: {
+					color:'#fff',
+					width:3
+				},
+				label: {
+					show:true,
+					radius:0.8,
+					background: {
+						opacity:0.7
+					},
+					threshold:0.05,
+					formatter: function(label, series){
+						var ll = '',
+							vr = new RegExp('[0-9]'),
+							vl = label.search(vr),
+							v = label.substr(vl),
+							b = label.substring(0,vl);
+						b = b.charAt(0).toUpperCase() + b.substr(1);
+
+						if(b == 'Msie') {
+							b = 'IE';
+						}
+						lc = b +' v.' + v + '<br />'+ Math.round(series.percent) +'%';
+						return lc;
+					}
+				},
+				combine: {
+					color:"#333",
+					threshold:0.05
+				}
+			}
+		},
+		legend: {
+			show:false
+		},
+		grid: {
+			hoverable:true
+		}
+	},
+	/*@2 OS*/
+	os: {
+		series: {
+			pie: {
+				show:true,	//we want to show a pie chart
+				radius:0.8,	//we want it to be smaller then the container, to get a better donut effect when you get the minor versions
+				stroke: {
+					color:'#fff',	//inner borders white
+					width:2
+				},
+				label: {
+					show:false,
+					radius:0.8,
+					background: {
+						opacity:0.2
+					},
+					threshold:0.05
+				},
+				combine: {
+					threshold:0.02,		//combine all slices that are under 5% of total
+					color:"#333333",	//make them color #333333
+					label:'All Others'
+				}
+			}
+		},
+		legend: {
+			show:true,		//show the legend
+			container: $("#os figcaption"),	//put the legend in the figcaption under the browser section
+			noColumns: 1,
+			labelFormatter: function(label, series){
+				var ll = '';
+				switch (label){
+					case 'win':
+						lc = 'Windows'
+						break;
+					case 'mac':
+						lc = 'Mac OSX';
+						break;
+					default:
+						lc = label.charAt(0).toUpperCase() + label.substr(1);
+				}
+				return lc + ' - ' + Math.round(series.percent) +"%";
+			}
+		},
+		grid: {
+			hoverable:true,
+			clickable:true
+		}
+	},
+	//OS minor
+	osMinor: {
+		series: {
+			pie: {
+//				innerRadius:0.55,
+				radius:0.9,
+				show:true,
+				stroke: {
+					color:'#fff',
+					width:3
+				},
+				label: {
+					show:true,
+					radius:0.8,
+					background: {
+						opacity:0.7
+					},
+					threshold:0.1,
+					formatter: function(label, series){
+						var ll = '',
+							vr = new RegExp('[0-9]'),
+							vl = label.search(vr),
+							v = label.substr(vl),
+							b = label.substring(0,vl);
+						b = b.charAt(0).toUpperCase() + b.substr(1);
+
+						if(b == 'Msie') {
+							b = 'IE';
+						}
+						lc = b +' v.' + v + '<br />'+ Math.round(series.percent) +'%';
+						return lc;
+					}
+				},
+				combine: {
+					color:"#333",
+					threshold:0.1
+				}
+			}
+		},
+		legend: {
+			show:false
+		},
+		grid: {
+			hoverable:true
+		}
+	},
+	
+	/*@2 Hourly*/
+	hourly: {
+		series: {
+			points: {
+				show: true,
+				symbol: "circle"
+			},
+			bars: {
+				points: {
+					show: false
+				}
+			}
+		},
+		xaxis: {
+			tickDecimals: 0
+			
+		},
+		 yaxis: {
+			 
+        },
+        grid: {
+			hoverable: true,
+			clickable:true
+        }
+	}
+};
