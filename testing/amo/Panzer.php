cffb90d9e8b8b6398b95c919e5ce5dc1ff3447c7
lord.xeon@gmail.com <lord.xeon@gmail.com@d8b2c144-dc4b-914c-d878-6766efc34fb8>
2011-04-20 02:27:38 +0000

    browser logic

--- testing/amo/Panzer.php
+++ testing/amo/Panzer.php
@@ -26,23 +26,36 @@ class Panzer extends nordlicht {
 	
 	public function builder(){
 		parent::builder();
+	}
+	
+	public function Browserbuilder(){
+		$this->builder();
 		$this->majorVersions = $this->sortTypes($this->mv);
-	//	$this->minorVersions = $this->sortTypes($this->mmv);
 	}
 	
-	public function debug(){
-		echo '<pre>';
-		//print_r($this->ss);
-		echo '</pre>';
+	
+	public function OSbuilder(){
+		$this->builder();
+		arsort($this->ss['OS ID']);
+		$this->majorVersions = $this->OSlist();
 	}
 	
+
 	public function destroy(){
 		parent::destroy();
-		$this->hRows = $this->iRows = '';
 		$this->tableMeta = $this->majorVersions = $this->minorVersions = array();
 		$total = array();
-
 	}
+	
+	
+	public function minorHold(){
+		echo json_encode($this->sortTypes($this->mmv));
+	}
+	
+	public function OSminor(){
+		echo json_encode($this->OSlist(true));
+	}
+	
 	
 	public function sortTypes($which){
 		$total = array();
@@ -66,17 +79,89 @@ class Panzer extends nordlicht {
 
 		return $total;
 	}
+	
+	
 	public function majorTable(){
+		$td = '';
 		echo "<table class=\"nStats\">\n";
-		echo "\t<tr>\n\t\t<th>Browser<th>\n\t\t<th>Hits</th>\n\t</tr>\n";
+		echo "\t<tr>\n\t\t<th>Browser</th>\n\t\t<th>Hits</th>\n\t</tr>\n";
+		
 		foreach($this->majorVersions as $b=>$h):
-			$td = "\t<tr>\n";
-			$td .= "\t\t\<td>".$b."</td>\n";
-			$td .= "\t\t<td>".$h."</td>\n";
-			$td = "\t</tr>\n";
+			$bn = '';
+			switch ($b):
+				case 'msie':
+					$bn = 'Internet Explorer';
+					break;
+				default:
+					$bn = strtoupper(substr($b,0,1)) . substr($b,1);
+			endswitch;
+			$bc = $this->checked[$b]['color'];
+			$td .= "\t<tr>\n";
+			$td .= "\t\t".'<td data-piecolor="'.$bc.'" data-piename="'.$b.'" data-pieval="'.$h.'">'."\n";
+			$td .= "\t\t\t".'<img src="images/browsers/'.$b.'.png" alt="'.$b.'"/>'.$bn."</td>\n";
+			$td .= "\t\t<td>".number_format($h)."</td>\n";
+			$td .= "\t</tr>\n";
 		endforeach;
 		echo $td;
 		echo "</table>\n";
+	}
+	
+	public function OStable(){
+		$td = '';
+		$i = 1;
+		echo "<table class=\"nStats\">\n";
+		echo "\t<tr>\n\t\t<th>Operating System</th>\n\t\t<th>Hits</th>\n\t</tr>\n";
+		foreach($this->majorVersions as $b=>$h):
+			if($i > $this->limit):
+				break;
+			endif;
+			
+			$bn = strtoupper(substr($b,0,1)) . substr($b,1);
+			$bc = $this->checked[$b]['color'];
+			
+			$td .= "\t<tr>\n";
+			$td .= "\t\t".'<td data-piecolor="'.$bc.'" data-piename="'.$b.'" data-pieval="'.$h.'">'.$bn."</td>\n";
+			$td .= "\t\t<td>".number_format($h)."</td>\n";
+			$td .= "\t</tr>\n";
+			$i++;
+		endforeach;
+		echo $td;
+		echo "</table>\n";
+	}
+	
+	
+	public function OSlist($skip=false){
+		$total = array();
+		foreach($this->ss['Hits'] as $id=>$h):
+			$osid = $this->ss['OS ID'][$id];
+			if($skip):
+				$total[$osid] = $h;
+			else:
+				$known = false;
+				foreach($this->mv as $os):
+					$inV = strpos($osid,$os);
+					if($inV !== false):
+						$total[$os] += $h;
+						$known = true;
+					endif;
+				endforeach;
+				if($known == false):
+					switch($osid):
+						case 'blackberry':
+							$total['mobile'] += $h;
+							break;
+						case 'symbian':
+							$total['mobile'] += $h;
+							break;
+						default:
+							$total[$osid] = $h;
+					endswitch;
+				endif;
+			endif;
+		endforeach;
+		arsort($total);
+		
+		return $total;
 	}
 	
 }
