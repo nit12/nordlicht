b1d94d21f6c0c7c1c260fd9f03fa5d01766e67c5
lord.xeon@gmail.com <lord.xeon@gmail.com@d8b2c144-dc4b-914c-d878-6766efc34fb8>
2011-04-23 20:08:38 +0000

    errors page

--- branches/v2/errors.php
+++ branches/v2/errors.php
@@ -1,35 +1,37 @@
-<section id="errors" class="statsSection">
-	<h2>Errors</h2>
-	<table> 
-		<tr> 
-			<th class="Errors">Errors</th> 
-			<th class="Type">Type</th> 
-			<th class="Hits">Hits</th> 
-			<th class="Bandwidth">Bandwidth</th> 
-		</tr> 
-		<tr> 
-			<td>404<span class="moreInfo" data-jsondata="404">?</span></td> 
-			<td>Not Found</td> 
-			<td>70</td> 
-			<td>21.00 KB</td> 
-		</tr> 
-		<tr> 
-			<td>206<span class="moreInfo" data-jsondata="206">?</span></td> 
-			<td>Partial Content</td> 
-			<td>32</td> 
-			<td>13.07 MB</td> 
-		</tr> 
-		<tr> 
-			<td>301<span class="moreInfo" data-jsondata="301">?</span></td> 
-			<td>Moved Permanently</td> 
-			<td>20</td> 
-			<td>6.22 KB</td> 
-		</tr> 
-		<tr> 
-			<td>416<span class="moreInfo" data-jsondata="416">?</span></td> 
-			<td>Requested Range Not Satisfiable</td> 
-			<td>4</td> 
-			<td>1.29 KB</td> 
-		</tr> 
-	</table>
-</section> <!-- closes errors section -->
+<?php
+require_once('amo/nordlicht.php');
+require_once('amo/Sherman.php');
+
+$sh = new Sherman();
+$sh->fi='amo/awstats032011.blog.deadlycomputer.com.txt';
+$sh->sec='ERRORS';
+$checked = array(
+	'Hits'=>array(
+		'color'=>'#090',
+		'type'=>'lines',
+		'yAxis'=>1,
+		'on'=>true
+	),
+	'Bandwidth'=>array(
+		'color'=>'#900',
+		'type'=>'bars',
+		'yAxis'=>2,
+		'on'=>true
+	));
+$sh->checked = $checked;
+$sh->typeMeta = $apacheStatusCode;
+$sh->tableMeta = array('Errors','Hits','Bandwidth');
+?>
+<section id="errors">
+	<aside class="tableSide">
+<?php $sh->builder(); ?>
+	</aside>
+	<aside class="plotSide">
+		<figure>
+			<div id="errorsPlot"></div>
+			<figcaption></figcaption>
+		</figure>
+	</aside>
+</section>
+
+<?php $sh->destroy(); ?>
